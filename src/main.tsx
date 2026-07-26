d7f7861a8f021364057429b298968c5a5e976231
Pepitodrop <66082927+Pepitodrop@users.noreply.github.com>
2026-07-26 16:50:18 +0200

    style: load usability controls

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { VaultGate } from './VaultGate'
 import './styles.css'
 import './ai.css'
 import './assistant.css'
+import './usability.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
